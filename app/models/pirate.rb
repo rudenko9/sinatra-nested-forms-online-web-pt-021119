378616502eb6daecd0ecc82fdb109a732098b6d6
Nataliya R <n.rudenko@gmail.com>
2019-05-28 19:55:13 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  
+ @@all = []
+ 
+  attr_accessor :name, :weight, :height
+  def initialize(hash)
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
+    
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+    
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+   @@all = []
+  attr_accessor :name, :type, :booty
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @type = hash[:type]
+    @booty = hash[:booty]
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear
+    @@all.clear
+  end 
+  
 end
